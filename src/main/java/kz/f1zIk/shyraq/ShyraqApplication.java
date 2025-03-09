82741bdfad488d2eaf142395b945f905f4a94cc5
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2025-03-09 21:45:11 +0500

    first commit

---
+++ src/main/java/kz/f1zIk/shyraq/ShyraqApplication.java
@@ -0,0 +1,13 @@
+package kz.f1zIk.shyraq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShyraqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShyraqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/f1zIk/shyraq/ShyraqApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.f1zIk.shyraq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShyraqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
